d1a555a8521391d4c22d17d5d430c91bfcbf723c
Audrey Pan <panaudrey09@gmail.com>
2025-01-31 01:58:14 -0600

    ghost finally moves
    Ghost is moving using scatter targets, but path is a little off so ghost just stops after getting to the first one

--- Scenes/ghost.gd
+++ Scenes/ghost.gd
@@ -13,8 +13,12 @@ func _ready():
 	call_deferred("setup")
 
 
-func scatter():
-	navigation_agent_2d.target_position = movement_targets.scatter_targets[current_scatter_index]
+func scatter(): 
+	var node = get_node_or_null(movement_targets.scatter_targets[current_scatter_index])
+	var target_path = movement_targets.scatter_targets[current_scatter_index]
+	print("Target NodePath:", target_path)
+	if node is Node2D:
+		navigation_agent_2d.target_position = node.global_position
 
 func _process(delta):
 	move_ghost(navigation_agent_2d.get_next_path_position(), delta)
@@ -22,18 +26,21 @@ func _process(delta):
 func move_ghost(next_position: Vector2, delta: float):
 	var current_ghost_position = global_position
 	
-	var new_velocity = (next_position - current_ghost_position).normalized() * speed * delta
+	var new_velocity = (next_position - current_ghost_position).normalized() * speed * delta;
 	position += new_velocity
 	
 
 func setup():
+	print(tile_map.get_navigation_map(0))
 	navigation_agent_2d.set_navigation_map(tile_map.get_navigation_map(0))
 	NavigationServer2D.agent_set_map(navigation_agent_2d.get_rid(),tile_map.get_navigation_map(0))
 	scatter()
 
 func on_position_reached():
 	if current_scatter_index < 3:
-		current_scatter_index +=1
+		current_scatter_index += 1
 	else:
-		current_scatter_index=0
-	navigation_agent_2d.target_position = movement_targets.scatter_targets[current_scatter_index]
+		current_scatter_index = 0
+	var node = get_node_or_null(movement_targets.scatter_targets[current_scatter_index])
+	if node is Node2D:
+		navigation_agent_2d.target_position = node.global_position
--- cheese.gd
+++ cheese.gd
@@ -5,9 +5,9 @@ extends Area2D
 signal cheese_collected(points)  # Signal to update score
 
 func _ready():
-    connect("body_entered", _on_mouse_collect)
+	connect("body_entered", _on_mouse_collect)
 
 func _on_mouse_collect(body):
-    if body.name == "Mouse":  # If the player (mouse) collides
-        cheese_collected.emit(points)  # Emit signal for score
-        queue_free()  # Remove the cheese pellet
+	if body.name == "Mouse":  # If the player (mouse) collides
+		cheese_collected.emit(points)  # Emit signal for score
+		queue_free()  # Remove the cheese pellet
