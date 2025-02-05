43f26b6389e969e9c0f39a1110357daab6466adc
Thapa-3097523 <143840451+asthathapa4@users.noreply.github.com>
2025-02-04 23:30:10 -0600

    Power pellets testing
    Power pellets test

--- Scenes/pellet.gd
+++ Scenes/pellet.gd
@@ -11,6 +11,30 @@ func _on_body_entered(body: Node2D) -> void:
 		pellet_eaten.emit(self)
 		queue_free()
 	
-	if should_allow_eating_ghosts:
+if should_allow_eating_ghosts:
+
+
+var pellets = [Vector2(100, 100), Vector2(200, 200), Vector2(300, 300), Vector2(400, 400)]
+var power_pellets = [Vector2(150, 150), Vector2(450, 450)]
+
+func _ready():
+	set_process(true)
+
+func _draw():
+	for pellet in pellets:
+		draw_circle(pellet, 5, Color(1, 1, 0))
+	for power_pellet in power_pellets:
+		draw_circle(power_pellet, 10, Color(1, 1, 1))
+
+func _process(delta):
+	for power_pellet in power_pellets:
+		if is_pacman_colliding(power_pellet):
+			power_pellets.erase(power_pellet)
+			update()
+
+func is_pacman_colliding(position):
+	var pacman_position = get_node("../PacMan").position
+	return pacman_position.distance_to(position) < 15
+
 		pass
 		
