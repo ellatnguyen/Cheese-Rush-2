a77e249cc3031594440e3743e21d1f29a4fd1868
Audrey Pan <panaudrey09@gmail.com>
2025-02-05 02:54:06 -0600

    added chasing cat, teleport cats after eating large pellet
    teleporting after large pellet is to test how the poofing function will work with the button mashing minigame

---
+++ Scenes/main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@onready var chase_cat = $Enemys/chase_cat
+@onready var scatter_cat = $Enemys/Scatter_cat
+
+func _ready() -> void:
+	# We will connect the signals from large pellets to _on_large_pellet_eaten()
+	# in code OR via the Editor (next steps).
+	pass
+
+func _on_large_pellet_eaten(pellet):
+	# This function is called when a large pellet is eaten.
+	# Teleport each cat to the cage for 7 seconds.
+	chase_cat.teleport_back_to_cage_for_7_seconds()
+	scatter_cat.teleport_back_to_cage_for_7_seconds()
+
+
+func _on_LargePellet_pellet_eaten(pallet: Pellet) -> void:
+	chase_cat.teleport_back_to_cage_for_7_seconds()
+	scatter_cat.teleport_back_to_cage_for_7_seconds()
